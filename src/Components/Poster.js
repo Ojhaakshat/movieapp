6c5af51094646c2181e15b6733723bb242f72224
Ojhaakshat <ojhaakshat123@gmail.com>
2022-01-25 00:05:30 +0530

    Mobile view

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -10,11 +10,8 @@ export default class Poster extends Component {
               </div>:
               <div className="card poster-card">
               <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}   alt={movie.title} className="card-img-top poster-img"/>
-              {/* <div className="card-body"> */}
-                  <h1 className="card-title poster-title">{movie.original_title}</h1>
-                  <p class="card-text poster-text">{movie.overview}</p>
-                  {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
-              {/* </div> */}
+              <h1 className="card-title poster-title">{movie.original_title}</h1>
+              <p className="card-text poster-text">{movie.overview}</p>
               </div>
             }
             </>);

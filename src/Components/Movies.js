0405a6ac2ed173b36d780d4c7507e1702608842c
Ojhaakshat <ojhaakshat123@gmail.com>
2022-01-25 02:19:35 +0530

    pagination

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -26,6 +26,17 @@ export default class Movies extends Component {
                                 ))
                             }
                         </div>
+                        <div style={{display:'flex',justifyContent:'center'}}>
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination">
+                                    <li className="page-item"><a className="page-link" >Previous</a></li>
+                                    <li className="page-item"><a className="page-link" >1</a></li>
+                                    <li className="page-item"><a className="page-link" >2</a></li>
+                                    <li className="page-item"><a className="page-link" >3</a></li>
+                                    <li className="page-item"><a className="page-link">Next</a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 }
             </>

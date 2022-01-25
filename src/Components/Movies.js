397758f7abf1a48141c453ff386fc5a5a01a604a
Ojhaakshat <ojhaakshat123@gmail.com>
2022-01-25 12:30:12 +0530

    state hover added

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { movies } from './getMovies';
 export default class Movies extends Component {
-  render() {
+    constructor() {
+        super();
+        this.state = {
+            hover: ''
+        }
+    }
+    render() {
         let movie = movies.results;
         return (
             <>
@@ -15,12 +21,15 @@ export default class Movies extends Component {
                         <div className="movies-list">
                             {
                                 movie.map((ele) => (
-                                    <div className="card movies-card">
+                                    <div className="card movies-card" onMouseEnter={() => this.setState({hover:ele.id})} onMouseLeave={()=>this.setState({hover:''})}>
                                         <img src={`https://image.tmdb.org/t/p/original${ele.backdrop_path}`}   alt={ele.title} className="card-img-top movies-img"/>
                                         <h1 className="card-title movies-title">{ele.original_title}</h1>
                                         {/* <p className="card-text poster-text">{ele.overview}</p> */}
                                         <div className="button-wrapper" style={{display:'flex',width:'100%',justifyContent:'center'}}>
-                                            <a className='btn btn-primary movies-button'>Use me</a>
+
+                                            {
+                                                this.state.hover == ele.id && <a className='btn btn-primary movies-button'>Add to Fav</a>
+                                            }
                                         </div>
                                     </div>
                                 ))

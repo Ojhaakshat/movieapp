15b474d9e70749fb3a47b228f6c69669d99660ec
Ojhaakshat <ojhaakshat123@gmail.com>
2022-01-25 20:44:21 +0530

    pagination complete and api calls

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
 import { movies } from './getMovies';
+import axios from 'axios';
 export default class Movies extends Component {
     constructor() {
         super();
         this.state = {
-            hover: ''
+            hover: '',
+            page_arr: [1],
+            currentPage: 1,
+            movies: []
+        }
+    }
+    async componentDidMount() {
+        //sideffects
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=c601404f39cfa24cd4b4a50dd0491459&page=${this.state.currentPage}`);
+        // console.log(res);
+        this.setState({
+            movies: [...res.data.results]
+        })
+        // console.log("Mounting done");
+    }
+    changeMovies = async ()=> {
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=c601404f39cfa24cd4b4a50dd0491459&page=${this.state.currentPage}`);
+        this.setState({
+            movies: [...res.data.results]
+        })
+    }
+    
+    
+    updateNext = () => {
+        let temparr = [];
+        for(let i = 1; i <= this.state.page_arr.length+1; i++) {
+            temparr.push(i);
+        }
+        //this.setState function is asynchronys, so wo pass changeMovies functions' definition as a callback to it, so that it is called only after setState function is complet
+        this.setState({
+            page_arr: [...temparr],
+            currentPage: this.state.currentPage + 1
+        }, this.changeMovies)
+        
+    }
+
+    updatePrev = () => {
+       if(this.state.currentPage != 1) {
+           this.setState({
+               currentPage: this.state.currentPage - 1
+           }, this.changeMovies)
+       }
+    }
+
+    setPage = (pg_no) => {
+        if(this.currentPage != pg_no) { 
+            this.setState({
+                currentPage: pg_no,
+            }, this.changeMovies)   
         }
     }
     render() {
-        let movie = movies.results;
+        // let movie = movies.results;
+        // console.log("Render done");
         return (
             <>
                 {
-                    movie.length == 0?
+                    this.state.movies.length == 0?
                     <div class="spinner-border text-primary" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>:
@@ -20,7 +70,7 @@ export default class Movies extends Component {
                         <h3 className='text-center'><strong>Trending</strong></h3>
                         <div className="movies-list">
                             {
-                                movie.map((ele) => (
+                                this.state.movies.map((ele) => (
                                     <div className="card movies-card" onMouseEnter={() => this.setState({hover:ele.id})} onMouseLeave={()=>this.setState({hover:''})}>
                                         <img src={`https://image.tmdb.org/t/p/original${ele.backdrop_path}`}   alt={ele.title} className="card-img-top movies-img"/>
                                         <h1 className="card-title movies-title">{ele.original_title}</h1>
@@ -38,11 +88,16 @@ export default class Movies extends Component {
                         <div style={{display:'flex',justifyContent:'center'}}>
                             <nav aria-label="Page navigation example">
                                 <ul className="pagination">
-                                    <li className="page-item"><a className="page-link" >Previous</a></li>
-                                    <li className="page-item"><a className="page-link" >1</a></li>
+                                    <li className="page-item"><a className="page-link" onClick={this.updatePrev}>Previous</a></li>
+                                    {/* <li className="page-item"><a className="page-link" >1</a></li>
                                     <li className="page-item"><a className="page-link" >2</a></li>
-                                    <li className="page-item"><a className="page-link" >3</a></li>
-                                    <li className="page-item"><a className="page-link">Next</a></li>
+                                    <li className="page-item"><a className="page-link" >3</a></li> */}
+                                    {
+                                        this.state.page_arr.map((ele)=>(
+                                            <li className="page-item"><a className="page-link" onClick={()=>this.setPage(ele)}>{ele}</a></li>
+                                        ))
+                                    }
+                                    <li className="page-item"><a className="page-link" onClick={this.updateNext}>Next</a></li>
                                 </ul>
                             </nav>
                         </div>
